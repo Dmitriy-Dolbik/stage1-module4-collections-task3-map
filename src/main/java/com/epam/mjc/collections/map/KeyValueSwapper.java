f658ec8248b563553baf11facdd601008eff0482
Dmitrii Dolbik <dda1508@gmail.com>
2023-01-19 14:30:34 +0300

    Add solution

--- src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
+++ src/main/java/com/epam/mjc/collections/map/KeyValueSwapper.java
@@ -1,8 +1,21 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
-public class KeyValueSwapper {
-    public Map<String, Integer> swap(Map<Integer, String> sourceMap) {
+public class KeyValueSwapper
+{
+    public Map<String, Integer> swap(Map<Integer, String> sourceMap)
+    {
+        Map<String, Integer> swappedMap = new HashMap<>();
+        for (Map.Entry<Integer, String> entry : sourceMap.entrySet())
+        {
+            String value = entry.getValue();
+            if (!swappedMap.containsKey(value) || entry.getKey() < swappedMap.get(value))
+            {
+                swappedMap.put(value, entry.getKey());
+            }
+        }
+        return swappedMap;
     }
 }
--- src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
+++ src/main/java/com/epam/mjc/collections/map/WordRepetitionMapCreator.java
@@ -1,8 +1,28 @@
 package com.epam.mjc.collections.map;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class WordRepetitionMapCreator {
+    public static void main(String[] args)
+    {
+        WordRepetitionMapCreator wordRepetitionMapCreator= new WordRepetitionMapCreator();
+        System.out.println(wordRepetitionMapCreator.createWordRepetitionMap("Hello,    world! How are you? hElLo"));
+    }
     public Map<String, Integer> createWordRepetitionMap(String sentence) {
+        Map<String, Integer> wordRepetitionMap = new HashMap<>();
+        if (sentence != null & sentence != ""){
+            String sentenceToLowerCase = sentence.toLowerCase();
+            String[] words = sentenceToLowerCase.split("[^a-zA-Z0-9']+");
+            for (String word : words) {
+                if (!wordRepetitionMap.containsKey(word)) {
+                    wordRepetitionMap.put(word, 1);
+                } else {
+                    int count = wordRepetitionMap.get(word);
+                    wordRepetitionMap.put(word, count + 1);
+                }
+            }
+        }
+        return wordRepetitionMap;
     }
 }
